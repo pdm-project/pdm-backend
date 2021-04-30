dfc9c5385e1ca77377648f57c1b7ca0caeed303b
林玮 (Jade Lin) <linw1995@icloud.com>
2021-04-30 22:35:06 +0800

    Harden existing test for testing file excluding in build dir

--- tests/test_api.py
+++ tests/test_api.py
@@ -185,8 +185,15 @@ def test_build_with_cextension(tmp_path):
         wheel_name = api.build_wheel(tmp_path.as_posix())
         sdist_name = api.build_sdist(tmp_path.as_posix())
 
-        tar_names = get_tarball_names(tmp_path / sdist_name)
-        assert "demo-package-0.1.0/my_package/hellomodule.c" in tar_names
-
         zip_names = get_wheel_names(tmp_path / wheel_name)
-        assert "my_package/hellomodule.c" not in zip_names
+        assert (
+            "my_package/hellomodule.c" not in zip_names
+        ), "Not collect c files while building wheel"
+
+        tar_names = get_tarball_names(tmp_path / sdist_name)
+        assert (
+            "demo-package-0.1.0/my_package/hellomodule.c" in tar_names
+        ), "Collect c files while building sdist"
+        assert not any(
+            path.startswith("build") for path in tar_names
+        ), 'Not collect c files in temporary directory "./build"'
