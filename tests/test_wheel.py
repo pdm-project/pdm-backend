d6323d10e83119ba5a64e5390a2e12d75965f632
Frost Ming <mianghong@gmail.com>
2021-05-06 15:05:35 +0800

    Fix test case

--- tests/test_wheel.py
+++ tests/test_wheel.py
@@ -1,5 +1,3 @@
-from unittest.mock import Mock
-
 from pdm.pep517 import wheel
 from tests import FIXTURES
 
@@ -7,7 +5,7 @@ from tests import FIXTURES
 def test_override_tags_in_wheel_filename(monkeypatch):
     project = FIXTURES / "projects/demo-cextension"
     builder = wheel.WheelBuilder(
-        project, config_settings={"--python-tag": "cp39", "--plat-name": "win_amd64"}
+        project,
+        config_settings={"--py-limited-api": "cp36", "--plat-name": "win_amd64"},
     )
-    monkeypatch.setattr(wheel, "get_abi_tag", Mock(return_value="cp39"))
-    assert builder.tag == "cp39-cp39-win_amd64"
+    assert builder.tag == "cp36-abi3-win_amd64"
