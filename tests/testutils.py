0cc8359f7593fd1207dfaa733e57e5b3bb6af1e7
Daniel Eades <danieleades@hotmail.com>
2021-11-28 08:19:50 +0000

    fix a few 'strict' mypy checks

--- tests/testutils.py
+++ tests/testutils.py
@@ -1,23 +1,25 @@
 import contextlib
 import tarfile
 import zipfile
+from pathlib import Path
+from typing import Iterator, List
 
 from pdm.pep517 import utils
 from tests import FIXTURES
 
 
-def get_tarball_names(path):
+def get_tarball_names(path: Path) -> List[str]:
     with tarfile.open(path, "r:gz") as tar:
         return tar.getnames()
 
 
-def get_wheel_names(path):
+def get_wheel_names(path: Path) -> List[str]:
     with zipfile.ZipFile(path) as zf:
         return zf.namelist()
 
 
 @contextlib.contextmanager
-def build_fixture_project(project_name):
+def build_fixture_project(project_name: str) -> Iterator[Path]:
     project = FIXTURES / "projects" / project_name
     with utils.cd(project):
         yield project
