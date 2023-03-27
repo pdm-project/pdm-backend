64117cf097bdbbd4f3bed6601393532658340dfb
Regen <regen100@users.noreply.github.com>
2023-03-27 10:24:11 +0900

    feat: Remove check on py-limited-api (#160)

--- tests/test_wheel.py
+++ tests/test_wheel.py
@@ -8,13 +8,26 @@ from tests import FIXTURES
 
 
 @pytest.mark.skipif(hasattr(sys, "pypy_version_info"), reason="invalid on PyPy3")
-def test_override_tags_in_wheel_filename() -> None:
+@pytest.mark.parametrize(
+    "python_tag, py_limited_api, plat_name, tag",
+    [
+        ("cp36", "abi3", "win_amd64", "cp36-abi3-win_amd64"),
+        ("py3", "none", "win_amd64", "py3-none-win_amd64"),
+    ],
+)
+def test_override_tags_in_wheel_filename(
+    python_tag: str, py_limited_api: str, plat_name: str, tag: str
+) -> None:
     project = FIXTURES / "projects/demo-cextension"
     with wheel.WheelBuilder(
         project,
-        config_settings={"--py-limited-api": "cp36", "--plat-name": "win_amd64"},
+        config_settings={
+            "--python-tag": python_tag,
+            "--py-limited-api": py_limited_api,
+            "--plat-name": plat_name,
+        },
     ) as builder:
-        assert builder.tag == "cp36-abi3-win_amd64"
+        assert builder.tag == tag
 
 
 def test_dist_info_name_with_no_version(tmp_path: Path) -> None:
