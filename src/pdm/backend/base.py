9b4cf4b63414e376f9f3528127bcdd6cf3d6e256
Frost Ming <mianghong@gmail.com>
2023-06-06 09:40:52 +0800

    Update src/pdm/backend/base.py

--- src/pdm/backend/base.py
+++ src/pdm/backend/base.py
@@ -181,14 +181,10 @@ class Builder:
     def build(self, build_dir: str, **kwargs: Any) -> Path:
         """Build the package and return the path to the artifact."""
         context = self.build_context(Path(build_dir), **kwargs)
-        should_clean = True
-
-        if self.config_settings.get("no-clean-build") is not None:
-            should_clean = not self.config_settings.get("no-clean-build")
-        elif os.getenv("PDM_BUILD_NO_CLEAN") is not None:
-            should_clean = os.getenv("PDM_BUILD_NO_CLEAN").lower() == "false"
-
-        if should_clean:
+        if not (
+            self.config_settings.get("no-clean-build")
+            or os.getenv("PDM_BUILD_NO_CLEAN", "false").lower() not in ("0", "false")
+        ):
             self.clean(context)
 
         self.initialize(context)
